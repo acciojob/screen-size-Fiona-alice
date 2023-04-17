bb854c5d8239f5e58f8698de3f45b0681f18e33e
Fiona-alice <120317574+Fiona-alice@users.noreply.github.com>
2023-04-17 16:51:40 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+  const sizeInfo = document.getElementById("sizeInfo");
+
+      const updateSize = () => {
+        const width = window.innerWidth;
+        const height = window.innerHeight;
+        sizeInfo.innerHTML = `<h1>Width: ${width} Height: ${height}</h1>`;
+      };
+	
+      window.addEventListener("resize", updateSize);
+updateSize();  
